f8b97ff1892257b0bfd72fc0abed7a24ca5ff0a1
Amorim da Silva <amorim@energiaautomacao.com.br>
2021-12-23 21:38:43 -0300

    routes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 
 import { ButtonHTMLAttributes } from 'react'
 
-// import '../styles/button.scss';
+import '../styles/button.scss';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 export function Button(props: ButtonProps) {
   return (
