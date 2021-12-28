29e32b2efe327d92fe8ea77c073cd3ab988728b1
amorimcode <bruno.amorim032@gmail.com>
2021-12-27 21:15:41 -0300

    listen questions

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,14 +1,22 @@
-import copyImg from '../assets/images/copy.svg'
+import copyImg from "../assets/images/copy.svg";
 
-import '../styles/room-code.scss'
+import "../styles/room-code.scss";
 
-export function RoomCode() {
-    return (
-        <button className="room-code">
-            <div>
-                <img src={copyImg} alt="Copy room code" />
-            </div>
-            <span>Sala #-MrxjjtX1vLSxcJM8ocQ</span>
-        </button>
-    )
+type RoomCodeProps = {
+  code: string;
+};
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
+  return (
+    <button className="room-code">
+      <div onClick={copyRoomCodeToClipboard}>
+        <img src={copyImg} alt="Copy room code" />
+      </div>
+      <span>Sala #{props.code}</span>
+    </button>
+  );
 }
