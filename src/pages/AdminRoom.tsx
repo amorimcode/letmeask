7ff8930a95e764cb9d922a83c4c4656666955516
amorimcode <bruno.amorim032@gmail.com>
2021-12-30 21:31:47 -0300

    likes fixed

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -61,36 +61,7 @@ export function AdminRoom() {
           </div>
         </div>
       </header>
-
       <main>
-        <div className="room-title">
-          <h1>Sala {title}</h1>
-          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
-        </div>
-
-        <form onSubmit={handleSendQuestion}>
-          <textarea
-            placeholder="O que você quer perguntar?"
-            onChange={(event) => setNewQuestion(event.target.value)}
-            value={newQuestion}
-          />
-
-          <div className="form-footer">
-            {user ? (
-              <div className="user-info">
-                <img src={user.avatar} alt={user.name} />
-                <span>{user.name}</span>
-              </div>
-            ) : (
-              <span>
-                Para enviar uma pergunta, <button>faça seu login</button>
-              </span>
-            )}
-            <Button type="submit" disabled={!user}>
-              Enviar pergunta
-            </Button>
-          </div>
-        </form>
         <div className="question-list">
           {questions.map((question) => {
             return (
